b4fe6f93d64b31f8a9f0e127c185ead58c72479c
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-02-16 06:15:23 -0800

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include"sort.h"
+/**
+  *bubble_sort - function that sort an array of ints
+  *bubble sort algo
+  *
+  *@array: array of integers
+  *@size: size of array
+  *
+  */
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, j;
+
+	if (!array || !size)
+	{
+		return;
+	}
+	i = 0;
+
+	while (i < size)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+
+}
